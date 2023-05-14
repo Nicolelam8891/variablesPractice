5ba966a5a24d177d0ed0578f8ba7e1467c186a80
Nicole Lam <Nicolelam8891@gmail.com>
2023-05-14 16:10:38 -0600

    add variables

---
+++ variables.js
@@ -0,0 +1,18 @@
+var name = "John"
+name = "Nicole"
+var food = "sushi"
+food = "wonton"
+var email = "john@gmail.com"
+email = "nicole@gmail.com"
+
+console.log(name)
+console.log(food)
+console.log(email)
+
+var age = 31
+var phoneNumber = 3039999999
+var license = 989765
+
+var blackHaair = true
+var married = true
+var citizen = true
